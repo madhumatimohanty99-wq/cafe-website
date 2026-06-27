3c7905b889f5d16f4b1f2f5e9a804d152c4f2a31
Madhumati Mohanty <madhumatimohanty99@gmail.com>
2026-06-27 18:36:28 +0530

    Fix cafe and background image rendering

---
+++ two.py
@@ -0,0 +1,45 @@
+# str1 = "this is string"
+# str2 = 'madhu'
+# str3 ="""that is"""
+# final_str = str1+" "+str2
+# print(final_str)
+# print(len(str1))
+# ch = str1[3]
+# print(ch)
+# print(str2[2:4])
+# print(str3[-4:-3])
+# print(str1.endswith("am"))
+# print(str1.find("o"))
+
+
+# name=input("enter your name :")
+# print("length of name is : ", len(name))
+
+# age=15
+# if(age >= 18):
+#     print("can vote")
+
+
+
+# list1 = [1,2,1]
+# list2 = [1,2,3]
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if(copy_list1 ==list1):
+#         print("palindrome")
+# else:
+#         print("not palindeome")
+
+
+# info = {
+#     "key" : "value",
+#     "name" : "mama",
+#     "learning" : "coding"
+# }
+# print(info)
+
+i = 100
+while i >= 1:
+    print(i)
+    i -= 100
